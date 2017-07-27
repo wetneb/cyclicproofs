dda2d76ded1bd3f46e59f6f17d3145e070d78090
Antonin Delpeuch <antonin@delpeuch.eu>
2017-07-27 18:49:50 +0100

    Add proof search

--- proof.py
+++ proof.py
@@ -1,5 +1,8 @@
-from proofstep import UnitAxiom, MergeStep
 from hashable_collections.hashable_collections import hashable_list
+from collections import defaultdict
+
+from proofstep import UnitAxiom, MergeStep
+from rbgraph import B, r
 
 class Proof(object):
     """
@@ -98,3 +101,75 @@ class Proof(object):
 
     def __hash__(self):
         return hash((self.hyp,self.steps))
+
+    @classmethod
+    def enumerate(cls, limit):
+        """
+        Enumerate all provable terms up to
+        a certain proof depth
+        """
+        reachable = {}
+        reachable[0] = {B(r):1}
+
+        all_reachables = {B(r)}
+
+        backtrack = defaultdict(list)
+        # structure of this dict:
+        # term -> list of (lhs,rhs,i,j,k,l)
+
+        # fill each set of reacheable terms after m merges
+        for m in range(1,limit+1):
+            reachable[m] = {}
+            # select a previous number of merges for the LHS
+            for p in range(0,m):
+                # loop through existing proofs for the LHS
+                for lhs, num_lhs in reachable[p].items():
+                    # loop through existing proofs for the RHS
+                    for rhs, num_rhs in reachable[m-1-p].items():
+                        # loop through possible merges
+                        for term,i,j,k,l in lhs.possible_merges(rhs):
+                            new_term = term not in all_reachables
+                            if term in reachable[m]:
+                                reachable[m][term] += num_lhs * num_rhs
+                                backtrack[term]
+                            elif new_term:
+                                reachable[m][term] = num_lhs * num_rhs
+                                all_reachables.add(term)
+
+                            if term in reachable[m]:
+                                backtrack[term].append(
+                                    (lhs,rhs,i,j,k,l))
+
+            #print('------{}------'.format(m))
+            #for term, count in reachable[m].items():
+            #    if term == triple_unit:
+            #        print('triple unit:')
+            #    print('{}\t{}'.format(count,term))
+        return backtrack
+
+    @classmethod
+    def reconstruct(cls, left, term, proof_of_left, backtrack):
+        """
+        Prints all proofs of a given term, at the context [left, X, right]
+        """
+        if term == B(r):
+            yield proof_of_left.unit(len(left))
+        else:
+            for lhs,rhs,i,j,k,l in backtrack[term]:
+                proofs_of_lhs = cls.reconstruct(left, lhs, proof_of_left.copy(), backtrack)
+                for proof_of_lhs in proofs_of_lhs:
+                    proofs_of_lhs_rhs = cls.reconstruct(left+(lhs,), rhs, proof_of_lhs, backtrack)
+                    for proof in proofs_of_lhs_rhs:
+                        yield proof.merge(len(left), (i,j,k,l))
+
+
+    def to_html(self):
+        html = ''
+        for j, step in enumerate(self.steps):
+            name = 'img/{}'.format('-'.join(str(t) for t in step.terms))
+            B.to_graphs(step.terms, 'output/'+name)
+            html += '<p>{}</p>\n'.format(step)
+            html += '<img src="{}.png" /><br />\n'.format(name)
+        return html
+
+
